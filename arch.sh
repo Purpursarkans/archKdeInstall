33c1a3d600f35a6c4d932a3702545089434ccf6f
MrXorek <goga.danilov10@gmail.com>
2019-12-31 23:50:42 +0400

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -5,7 +5,7 @@ mkdir -p /mnt/boot
 mount /dev/sda1 /mnt/boot
 mkswap /dev/sda2 -L "swap"
 swapon /dev/sda2
-echo -e "#Russia\nServer = https://mirror.rol.ru/archlinux/\$repo/os/\$arch\nServer = https://mirror.yandex.ru/archlinux/$repo/os/$arch" >> /etc/pacman.d/mirrorlist1
+echo -e "#Russia\nServer = https://mirror.rol.ru/archlinux/\$repo/os/\$arch\nServer = https://mirror.yandex.ru/archlinux/\$repo/os/\$arch" >> /etc/pacman.d/mirrorlist1
 cat /etc/pacman.d/mirrorlist >> /etc/pacman.d/mirrorlist1
 mv /etc/pacman.d/mirrorlist1 /etc/pacman.d/mirrorlist
 pacman -Syy
