42238e39404845c0867670291da704feae6842b2
MrXorek <goga.danilov10@gmail.com>
2020-01-02 17:45:18 +0400

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -12,27 +12,4 @@ rm /etc/pacman.d/mirrorlist1
 pacman -Syy
 pacstrap /mnt base linux linux-firmware base-devel
 genfstab -L -p -P /mnt >> /mnt/etc/fstab
-arch-chroot /mnt 
-pacman -S nano neofetch screenfetch mc
-loadkeys ru
-setfont cyr-sun16
-echo -e "en_US.UTF-8 UTF-8\nru_RU.UTF-8 UTF-8" >> nano /etc/locale.gen
-locale-gen
-echo "LANG=ru_RU.UTF-8" >> /etc/locale.conf
-export LANG=ru_RU.UTF-8
-echo -e "KEYMAP=ru\nFONT=cyr-sun16" >> /etc/vconsole.conf
-ln -sf /usr/share/zoneinfo/Europe/Saratov /etc/localtime
-hwclock --systohc
-echo "potato-pc" >> /etc/hostname
-echo -e "127.0.0.1	localhost\n::1			localhost\n127.0.1.1	potato-pc.localdomain	potato-pc" >> /etc/hosts 
-useradd -G wheel -s /bin/bash -m igorek
-echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers1
-cat /etc/sudoers >> /etc/sudoers1
-mv /etc/sudoers1 /etc/sudoers
-rm /etc/sudoers1
-passwd
-passwd igorek
-pacman -S  efibootmgr iw wpa_supplicant dialog netctl dhcpcd
-bootctl install
-echo -e "default arch\ntimeout 5\neditor 1" >> /boot/loader/loader.conf
-echo -e "title Arch Linux\nlinux /vmlinuz-linux\ninitrd /initramfs-linux.img\noptions root=/dev/sda3 rw" >> /boot/loader/entries/arch.conf
+arch-chroot /mnt sh -c "$(curl -fsSL git.io/JexzX)"
