c2e89b38197cf2aab4fe4f9975de7bd3e9bf55ad
MrXorek <goga.danilov10@gmail.com>
2020-01-07 10:39:05 +0000

    Update arch2.sh

--- arch2.sh
+++ arch2.sh
@@ -17,9 +17,8 @@ echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers1
 cat /etc/sudoers >> /etc/sudoers1
 mv /etc/sudoers1 /etc/sudoers
 pacman -S  efibootmgr iw wpa_supplicant dialog netctl dhcpcd --noconfirm
-systemctl enable NetworkManager
 bootctl install
-echo -e "default arch\ntimeout 5\neditor 1" >> /boot/loader/loader.conf
+echo -e "default arch\ntimeout 0\neditor 1" >> /boot/loader/loader.conf
 echo -e "title Arch Linux\nlinux /vmlinuz-linux\ninitrd /initramfs-linux.img\noptions root=/dev/sda3 rw" >> /boot/loader/entries/arch.conf
 clear
 echo "Введите пароль для рута"
