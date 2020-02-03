ba3e9ce772818c57599c17b514ff9e833db40ba4
Igor <goga.danilov10@gmail.com>
2020-02-03 22:52:06 +0400

    Update arch2.sh

--- arch2.sh
+++ arch2.sh
@@ -24,7 +24,7 @@ echo -e "title Arch Linux\nlinux /vmlinuz-linux\ninitrd /initramfs-linux.img\nop
 pacman -S xorg-server xorg-xinit --noconfirm
 echo "exec startkde" > ~/.xinitrc
 pacman -S plasma --noconfirm
-pacman -S konsole dolphin firefox kate ark aspell-ru telegram-desktop spectacle gimp --noconfirm
+pacman -S konsole dolphin firefox kate ark aspell-ru telegram-desktop spectacle gimp htop --noconfirm
 
 echo -e "[multilib]\nInclude = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
 pacman -S steam alsa-utils discord --noconfirm
