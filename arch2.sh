8d179a8a177bdf0c4bbb3debdf101baced0b56b9
Igor <goga.danilov10@gmail.com>
2020-02-03 21:58:40 +0400

    Update arch2.sh

--- arch2.sh
+++ arch2.sh
@@ -32,7 +32,7 @@ pacman -S xf86-video-ati mesa lib32-mesa
 systemctl enable sddm
 systemctl enable NetworkManager
 cd ~
-ecgo "export EDITOR=nano" >> .bashrc
+echo "export EDITOR=nano" >> .bashrc
 clear
 echo "Введите пароль для рута"
 passwd
