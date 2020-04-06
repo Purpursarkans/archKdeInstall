9388210992593164625003143c124ca6e9e1082f
Igor <goga.danilov10@gmail.com>
2020-04-07 02:15:16 +0400

    Update arch2.sh

--- arch2.sh
+++ arch2.sh
@@ -33,7 +33,7 @@ pacman -Suy gwenview kimageformats kipi-plugins kamera konsole dolphin firefox k
 
 pacman -Rs discover --noconfirm
 
-pacman -S xf86-video-ati mesa lib32-mesa --noconfirm
+pacman -S xf86-video-ati mesa lib32-mesa mesa-vdpau lib32-mesa-vdpau --noconfirm
 pacman -S earlyoom neofetch kdekonnect mpv p7zip virtualbox virtualbox-host-modules-arch blender krita audacity kdevelop cmake qbittorrent inkscape --noconfirm
 modprobe vboxdrv
 pacman -S dkms virtualbox-guest-utils virtualbox-guest-modules-arch --noconfirm
