8d3b9faa8dbbbb978e6ad8bef1082a3cb179d4f5
Igor <goga.danilov10@gmail.com>
2020-05-01 18:21:22 +0400

    Update arch_after.sh

--- arch_after.sh
+++ arch_after.sh
@@ -2,3 +2,8 @@
 
 yay -S minecraft-launcher amidst aseprite magicavoxel logmein-hamachi xournal
 sudo systemctl enable --now logmein-hamachi
+sudo hamachi login
+sudo hamachi set-nick "уточка"
+(
+echo;
+) | sudo hamachi join 403-695-568
