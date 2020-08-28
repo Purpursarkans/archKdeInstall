dbedfe279b7c588660741e45788a65244bd23c7f
Igor <goga.danilov10@gmail.com>
2020-08-28 17:27:16 +0400

    Update arch_after.sh

--- arch_after.sh
+++ arch_after.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yay -S minecraft-launcher amidst aseprite magicavoxel logmein-hamachi corectrl cpupower-gui radeontop --noconfirm
+yay -S minecraft-launcher amidst aseprite magicavoxel logmein-hamachi corectrl cpupower-gui radeontop wd719x-firmware aic94xx-firmware --noconfirm
 sudo systemctl enable --now logmein-hamachi
 sudo hamachi login
 sudo hamachi set-nick "уточка"
